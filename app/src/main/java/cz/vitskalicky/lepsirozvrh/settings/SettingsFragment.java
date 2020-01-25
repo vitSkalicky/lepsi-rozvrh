acb66ec81d5ebe9c71552a9c11850ba193178581
Vít Skalický <vit.skalicky@email.cz>
2020-01-25 17:09:37 +0100

    Removed proprietary 'play-services-oss-licenses' library.

--- app/src/main/java/cz/vitskalicky/lepsirozvrh/settings/SettingsFragment.java
+++ app/src/main/java/cz/vitskalicky/lepsirozvrh/settings/SettingsFragment.java
@@ -20,7 +20,6 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreferenceCompat;
 
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.io.FileInputStream;
@@ -88,12 +87,6 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             ad.show();
             return true;
         });
-        findPreference(getString(R.string.PREFS_OSS_LICENCES)).setOnPreferenceClickListener(preference -> {
-            Intent intent = new Intent(getContext(), OssLicensesMenuActivity.class);
-            startActivity(intent);
-            return true;
-        });
-
 
         Preference userInfo = findPreference(getString(R.string.PREFS_USER));
         userInfo.setTitle(SharedPrefs.getString(getContext(), SharedPrefs.NAME));
